20f92d80dad9ce8bd5cdce147e6667d94d96c3af
lixl <lixl@shterm.com>
2017-05-02 11:18:43 +0800

    update xshell_gen

--- xshell_session_gen/main.py
+++ xshell_session_gen/main.py
@@ -1,29 +1,32 @@
 # -*- coding: utf-8 -*-
+import getopt
 import os
 import sys
-import getopt
-import re
 
-sessions_dir = os.path.join(os.path.expanduser("~"), "Documents\NetSarang\Xshell\Sessions")
+sessions_dir = os.path.join(os.path.expanduser("~"), r"Documents\NetSarang\Xshell\Sessions")
+
 
 def get_template():
     with open("session_template.txt") as f:
             template = f.read()
             return template
 
-def save_session(session_name, template):
+
+def save_session(session_name, template, force):
     session_filepath = os.path.join(sessions_dir, session_name + ".xsh")
-    if not os.path.exists(session_filepath):
+    if not os.path.exists(session_filepath) or force:
         with open(session_filepath, "w") as f:
             f.write(template)
         print("save successful: " + session_name)
     else:
         print(session_name + " exists: " + session_name)
 
+
 def clear_empty(l):
     for x in l:
         if isinstance(x, str) and not x.strip():
             l.remove(x)
+
 
 def parse_host_re(host_base, dynamic):    
     hosts = []
@@ -36,13 +39,11 @@ def parse_host_re(host_base, dynamic):
         hosts.extend(parse_host_re(host_base.format(x), dynamic))
     return hosts        
 
+
 def parse_host(host_str):
     hosts = []
     host_base_dynamic = host_str.split(":", 1)
-    clear_empty(host_base_dynamic)    
-
-    import pdb
-    #pdb.set_trace()
+    clear_empty(host_base_dynamic)
 
     if len(host_base_dynamic) == 2:
         host_base = host_base_dynamic[0]
@@ -54,6 +55,7 @@ def parse_host(host_str):
             hosts.append(host_base_dynamic[0])
     return hosts
 
+
 def check_host(host):
     tmp = host.split(".")
     if len(tmp) != 4:
@@ -63,10 +65,9 @@ def check_host(host):
             num = int(x)
             if num < 0 or num > 255:
                 return False
-        except:
+        except Exception as e:
             return False
     return True
-                    
 
 
 def get_user_key(is_old=False):
@@ -74,6 +75,7 @@ def get_user_key(is_old=False):
         return "RSA-200610-openssh"
     else:
         return "RSA-201608-openssh"
+
 
 def get_port(args):
     port = args.get("port")
@@ -84,6 +86,7 @@ def get_port(args):
             port = 8022
     return port
 
+
 def main():
     options = getopt.getopt(sys.argv[1:], "h:p:", ["old"])[0]
     args = {}
@@ -93,10 +96,13 @@ def main():
         elif opt == "-p":
             args["port"] = int(val)
         elif opt == "--old":
-            args["old"] = val        
+            args["old"] = val
+        elif opt == "-f":
+            args["force"] = True
     print(args)
     user_key = get_user_key(args.get("old", False))    
     port = get_port(args)
+    force = args.get("force", True)
 
     for host in args["host"]:
         print("port: %s" % port)
@@ -104,7 +110,8 @@ def main():
         name = host.split(".", 2)[-1]
         template = get_template()        
         template = template.format(Host=host, Port=port, UserKey=user_key) 
-        save_session(name, template)
+        save_session(name, template, force)
+        print("===========\n")
 
 if __name__ == "__main__":
     """usage: -h10.10.16.21*:1,2,3,4,5 -p8022 --old
